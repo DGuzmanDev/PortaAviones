611d9ffcc692dc0c49c78e8bb43901c8ce99e7d7
dx006328 <daniel.guzman@maxar.com>
2023-11-29 17:40:34 -0600

    Se agregan las constantes para las marcas y modelos de aviones

---
+++ wwwroot/js/constants.js
@@ -0,0 +1,51 @@
+const aviones = [
+    {
+        marca: "Lockheed Martin",
+        modelos: [
+            "F-35 Lightning II",
+            "F-22 Raptor",
+            "SR-71 Blackbird",
+            "F-16 Fighting Falcon"
+        ]
+    },
+    {
+        marca: "Sukhoi",
+        modelos: [
+            "Su-35",
+            "Su-37",
+            "Su-39",
+            "Su-47",
+            "Su-57"
+        ]
+    },
+    {
+        marca: "Boeing",
+        modelos: [
+            "F/A-18 Super Hornet",
+            "F/A-18E/F Advanced Super Hornet II",
+            "F-15SG Strike Eagle",
+            "F-15K Slam Eagle",
+            "F-15SE Silent Eagle"
+        ]
+    },
+    {
+        marca: "Dassault",
+        modelos: [
+            "Rafale F3",
+            "Neuron",
+            "Mirage 2000C",
+            "Mirage 4000",
+            "Falcon Guardian"
+        ]
+    },
+    {
+        marca: "Saab",
+        modelos: [
+            "Draken",
+            "Gripen",
+            "Viggen 37",
+            "Lansen 32",
+            "Tunnan 29"
+        ]
+    }
+];
