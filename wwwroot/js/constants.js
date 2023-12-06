ea311d4ea2b61e6e7ca5f9aefbcfe1affee7f5c7
Daniel Guzmán Chaves <danig.ch@hotmail.com>
2023-12-05 20:01:39 -0600

    Se agrega la funcionalidad FE para la pantalla de inventario (#5)
    Co-authored-by: dx006328 <daniel.guzman@maxar.com>

--- wwwroot/js/constants.js
+++ wwwroot/js/constants.js
@@ -50,3 +50,102 @@ const aviones = [
         ]
     }
 ];
+
+const inventario_sinstesis = [
+    {
+        cuenta: 10,
+        marca: "Lockheed Martin",
+        modelo: "F-35 Lightning II"
+    },
+    {
+        cuenta: 12,
+        marca: "Lockheed Martin",
+        modelo: "F-22 Raptor"
+    },
+    {
+        cuenta: 8,
+        marca: "Dassault",
+        modelo: "Rafale F3"
+    }, {
+        cuenta: 5,
+        marca: "Saab",
+        modelo: "Draken"
+    }
+    , {
+        cuenta: 10,
+        marca: "Boeing",
+        modelo: "F/A-18E/F Advanced Super Hornet II"
+    }
+    , {
+        cuenta: 2,
+        marca: "Sukhoi",
+        modelo: "Su-57"
+    }
+];
+
+const inventario = [
+    {
+        marca: "Lockheed Martin",
+        modelo: "F-35 Lightning II",
+        serie: "Serie 1",
+        nombre: "Nombre 1",
+        fecha_ingreso: "2023/12/07 19:00:00 P.M",
+        tecnico: "Tecnico 1"
+    },
+    {
+        marca: "Lockheed Martin",
+        modelo: "F-35 Lightning II",
+        serie: "Serie 2",
+        nombre: "Nombre 1",
+        fecha_ingreso: "2023/12/07 19:00:00 P.M",
+        tecnico: "Tecnico 2"
+    },
+    {
+        marca: "Lockheed Martin",
+        modelo: "F-35 Lightning II",
+        serie: "Serie 3",
+        nombre: "Nombre 3",
+        fecha_ingreso: "2023/12/07 19:00:00 P.M",
+        tecnico: "Tecnico 3"
+    },
+    {
+        marca: "Sukhoi",
+        modelo: "Su-57",
+        serie: "Serie 4",
+        nombre: "Nombre 4",
+        fecha_ingreso: "2023/12/07 19:00:00 P.M",
+        tecnico: "Tecnico 4"
+    },
+    {
+        marca: "Sukhoi",
+        modelo: "Su-57",
+        serie: "Serie 5",
+        nombre: "Nombre 5",
+        fecha_ingreso: "2023/12/07 19:00:00 P.M",
+        tecnico: "Tecnico 5"
+    },
+    {
+        marca: "Boeing",
+        modelo: "F/A-18E/F Advanced Super Hornet II",
+        serie: "Serie 6",
+        nombre: "Nombre 6",
+        fecha_ingreso: "2023/12/07 19:00:00 P.M",
+        tecnico: "Tecnico 6"
+    },
+    {
+        marca: "Boeing",
+        modelo: "F/A-18E/F Advanced Super Hornet II",
+        serie: "Serie 7",
+        nombre: "Nombre 7",
+        fecha_ingreso: "2023/12/07 19:00:00 P.M",
+        tecnico: "Tecnico 7"
+    },
+    {
+        marca: "Boeing",
+        modelo: "F/A-18E/F Advanced Super Hornet II",
+        serie: "Serie 8",
+        nombre: "Nombre 8",
+        fecha_ingreso: "2023/12/07 19:00:00 P.M",
+        tecnico: "Tecnico 8"
+    }
+];
