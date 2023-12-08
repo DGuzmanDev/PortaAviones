e14e816d61b5a10e94946a5bb77a52f851e5d756
dx006328 <daniel.guzman@maxar.com>
2023-12-07 21:39:49 -0600

    Se completa la funcionalidad JS basica para los requerimientos de despegue

--- wwwroot/js/misiones.js
+++ wwwroot/js/misiones.js
@@ -1,5 +1,7 @@
 const DESPEGUE = "1";
 const ATERRIZAJE = "2";
+const EVENTO_LIMPIAR_DESPEGUE = 'total_despegue';
+const EVENTO_LIMPIAR_ATERRIZAJE = 'total_aterrizaje';
 
 var despegue = {
     aeronaves: []
@@ -17,6 +19,7 @@ function mostrar_resultados_busqueda_aeronave(show_duration) {
 }
 
 function limpiar_despegue() {
+    despegue = { aeronaves: [] };
     ocultar_resultados_busqueda_aeronave(0);
     $('#formulario_despegue')[0].reset();
     $($('#formulario_despegue')[0]).removeClass('was-validated');
@@ -25,22 +28,23 @@ function limpiar_despegue() {
     $('#formulario_piloto')[0].reset();
     $($('#formulario_piloto')[0]).removeClass('was-validated');
     $('#tabla_despegue_body').html('');
+    configurar_limite_fechas();
 }
 
 function limpiar_aterrizaje() {
+    aterrizaje = {};
     $('#resultados_aterrizaje').hide();
-    $('#formulario_busqueda_aterraizaje')[0].reset();
-    $($('#formulario_busqueda_aterraizaje')[0]).removeClass('was-validated');
+    $('#formulario_busqueda_aterrizaje')[0].reset();
+    $($('#formulario_busqueda_aterrizaje')[0]).removeClass('was-validated');
     $('#formulario_datos_aterrizaje')[0].reset();
     $($('#formulario_datos_aterrizaje')[0]).removeClass('was-validated');
     $('#tabla_aterrizaje_body').html('');
+    configurar_limite_fechas();
 }
 
 function registrar_evento_operacion() {
     $('#tipo_opercion').on('change', function (event) {
         let operacion = this.value;
-        despegue = { aeronaves: [] };
-        aterrizaje = {};
 
         switch (operacion) {
             case DESPEGUE:
@@ -61,6 +65,37 @@ function registrar_evento_operacion() {
                 limpiar_aterrizaje();
                 limpiar_despegue();
         }
+    });
+}
+
+function configurar_limite_fechas() {
+    var min = new Date();
+    var minHour = min.getHours() < 10 ? "0" + min.getHours() : min.getHours();
+    var minMinutes = min.getMinutes() < 10 ? "0" + min.getMinutes() : min.getMinutes();
+    var minDay = min.getDate() < 10 ? "0" + min.getDate() : min.getDate();
+    var minMonth = (min.getMonth() + 1) < 10 ? "0" + (min.getMonth() + 1) : (min.getMonth() + 1);
+
+    $("#FechaDespegue").val(min.getFullYear() + "-" + minMonth + "-" + minDay + "T" + minHour + ":" + minMinutes);
+    $("#FechaDespegue").attr("min", min.getFullYear() + "-" + minMonth + "-" + minDay + "T" + minHour + ":" + minMinutes);
+    $("#FechaAterrizaje").val(min.getFullYear() + "-" + minMonth + "-" + minDay + "T" + minHour + ":" + minMinutes);
+    $("#FechaAterrizaje").attr("min", min.getFullYear() + "-" + minMonth + "-" + minDay + "T" + minHour + ":" + minMinutes);
+}
+
+function registrar_evento_modal_limpiar() {
+    $('#modal_confirm').on('show.bs.modal', function (event) {
+        let button = event.relatedTarget;
+        let tipo_evento = button.getAttribute('data-bs-target-value');
+        registrar_evento_limpiar_formulario(tipo_evento);
+    });
+}
+
+function registrar_evento_limpiar_formulario(tipo) {
+    $('#limpiar_confirm').off('click');
+    $('#limpiar_confirm').on('click', function (event) {
+        if (tipo === EVENTO_LIMPIAR_DESPEGUE) {
+            limpiar_despegue();
+        }
+        $("#close_confirm").trigger("click");
     });
 }
 
@@ -148,10 +183,42 @@ function registrar_evento_buscar_aeronave() {
     });
 }
 
+function validar_datos_despegue() {
+    var tecnico = $('#Tecnico').val();
+    var mision = $('#Mision').val();
+    var fecha = $('#FechaDespegue').val();
+
+    return ((tecnico !== undefined && tecnico.trim() !== '')
+        && (mision !== undefined && mision.trim() !== '')
+        && (fecha !== undefined && fecha.trim() !== ''));
+}
+
+function registrar_evento_guardar_despegue() {
+    $('#enviar_despegue').on('click', function (event) {
+        var formDespegue = $('#formulario_despegue')[0];
+        $(formDespegue).addClass('was-validated');
+
+        if (formDespegue.checkValidity() && validar_datos_despegue()) {
+            if (despegue.aeronaves.length > 0) {
+                // TODO: Enviar el request al back-end con el modelo despegue
+                limpiar_despegue();
+                animate_feedback('exito_registro', 5000, 500, 500);
+            } else {
+                animate_feedback('error_aeronaves_despegue', 5000, 500, 500);
+            }
+        } else {
+            animate_feedback('error_formulario_despegue', 5000, 500, 500);
+        }
+    });
+}
+
 $(document).ready(function () {
     console.log('misiones.js JavaScript - Daniel Guzman Chaves - Programación Avanzanda en Web -  UNED IIIQ 2023');
+    configurar_limite_fechas();
+    registrar_evento_modal_limpiar();
     registrar_evento_operacion();
     registrar_evento_buscar_aeronave();
+    registrar_evento_guardar_despegue();
 });
 
 // var action_buttons = '<td>'
