f2cc92c8fae578cf48e1444a34bcee2a4b8622a3
dx006328 <daniel.guzman@maxar.com>
2023-12-10 08:35:31 -0600

    Ajuste al modelo de Retiro y Perdida

--- Models/Perdida.cs
+++ Models/Perdida.cs
@@ -2,13 +2,11 @@ namespace PortaAviones.Models
 {
     public class Perdida
     {
-        public int? Id { get; set; }
         public int? PerdidaHumana { get; set; }
         public Boolean? PerdidaMaterial { get; set; }
 
-        public Perdida(int? id, Boolean? perdidaMaterial, int? perdidaHumana)
+        public Perdida(Boolean? perdidaMaterial, int? perdidaHumana)
         {
-            Id = id;
             PerdidaMaterial = perdidaMaterial;
             PerdidaHumana = perdidaHumana;
         }
--- Models/Retiro.cs
+++ Models/Retiro.cs
@@ -2,17 +2,13 @@ namespace PortaAviones.Models
 {
     public class Retiro
     {
-        public int? Id { get; set; }
         public string? Tecnico { get; set; }
-        public string? Detalle { get; set; }
-        public DateTime? FechaRegistro { get; set; }
+        public List<Aeronave>? Aeronaves{get; set;}
 
-        public Retiro(int? id, string? tecnico, string? detalle, DateTime? fechaRegistro)
+        public Retiro(string? tecnico, List<Aeronave>? aeronaves)
         {
-            Id = id;
             Tecnico = tecnico;
-            Detalle = detalle;
-            FechaRegistro = fechaRegistro;
+            Aeronaves = aeronaves;
         }
     }
 }
